b1ce11cae0f0f506cf25081082551187b9b4425b
lasha giorgashvili <159286150+lasha-777@users.noreply.github.com>
2024-03-04 18:37:10 +0200

    lash

---
+++ app.js
@@ -0,0 +1,31 @@
+$(".menu-trigger").click(function(){
+    $(".menu").toggleClass("menu-visible");
+  });
+  
+  $(".close").click(function(){
+    $(".menu").toggleClass("menu-visible");
+  });
+  
+  $(".platofuerte-trigger").click(function(){
+    $(".fuertes").toggleClass("fuertes-visible");
+  });
+  
+  $(".cerrar2-trigger").click(function(){
+    $(".fuertes").toggleClass("fuertes-visible");
+  });
+  
+  $(".marbar-trigger").click(function(){
+    $(".mar").toggleClass("mar-visible");
+  });
+  
+  $(".cerrar-trigger").click(function(){
+    $(".mar").toggleClass("mar-visible");
+  });
+  
+  $(".ensaladas-trigger").click(function(){
+    $(".sopas").toggleClass("sopas-visible");
+  });
+  
+  $(".cerrar3-trigger").click(function(){
+    $(".sopas").toggleClass("sopas-visible");
+  });
